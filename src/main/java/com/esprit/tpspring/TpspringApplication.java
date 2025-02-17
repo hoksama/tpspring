64fab0cd908acbad92e7bcc6fcc711ca0ca22158
hoksama <ahmed.bh@live.fr>
2025-02-17 23:21:31 +0100

    Initial commit

---
+++ src/main/java/com/esprit/tpspring/TpspringApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.tpspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esprit/tpspring/TpspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.tpspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
